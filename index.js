a7bad3bd1fd4e3be67e911c27b79ad6ae7cb444b
necco <42417423+necco13@users.noreply.github.com>
2020-12-10 23:09:42 +0100

    Bot

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+
+bot.login('Nzg2Njg5MjAzNjgwMzEzMzg0.X9KDag.87Ey548PzLLG8nWJ1faIVDb1ttw'); //Login s tokenom
+
+bot.on('ready', () => {     //po startupu
+  console.info("Tale bullshit se je zalaufu"); //Welcoming message
+});
+
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.channel.send('pinging your mama');
+  }
+
+});
+
+function help()
+{
+  msg.channel.send("Rules can be found in");
+}
